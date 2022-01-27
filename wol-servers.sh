a46efe2e5f838b44da73867fc2e842f8055f6240
Andrei Sfat <andrei@sfat.me>
2022-01-27 23:06:49 +0200

    Update wol-servers.sh

--- wol-servers.sh
+++ wol-servers.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # Script to wake up servers on a scheduled basis using WoL (Wake on Lan) to power on servers in the morning, 
 # while the servers themselves have a cron task that shutdown themselves at midnight (5 minutes after midnight to be more precise)
@@ -7,7 +7,9 @@
 # this runs once a day
 # run
 # crontab -e
-# 0 9 * * * bash /home/pi/wol-servers.sh "${#SERVER_HOSTS[@]}" "${#SERVER_MACADDRS[@]}" > wol-servers.log
+# 0 9 * * * bash /home/pi/wol-servers.sh \
+                                        <( (( ${#SERVER_HOSTS[@]} )) && printf '%s\0' "${SERVER_HOSTS[@]}") \
+                                        <( (( ${#SERVER_MACADDRS[@]} )) && printf '%s\0' "${SERVER_MACADDRS[@]}") > wol-servers.log
 # e.g:
 # SERVER_HOSTS=(192.168.0.1 192.168.0.2 192.168.0.3)
 # SERVER_MACADDRS=(mac.address.1 mac.address.2 mac.address.3)
@@ -24,8 +26,9 @@ if [ "" = "$PKG_OK" ]; then
   sudo apt-get --yes install $REQUIRED_PKG
 fi
 
-declare -n HOSTS=$1
-declare -n MACADDRS=$2
+# bash 4.4 or newer
+mapfile -d '' HOSTS <"$1"
+mapfile -d '' MACADDRS <"$2"
 
 HOSTNUM=0
 HOSTSDOWN=0
